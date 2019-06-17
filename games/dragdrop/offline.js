dab14842ceae7334e54526186693a19342d65f48
Renata Kilian <renatakiliandev@gmail.com>
2019-06-17 17:55:37 -0300

    Updating CSS, adding images game files

---
+++ games/dragdrop/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1556753345,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/peca_destino-sheet0.png",
+		"images/peca_destino-sheet1.png",
+		"images/peca_origem-sheet0.png",
+		"images/particles.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
