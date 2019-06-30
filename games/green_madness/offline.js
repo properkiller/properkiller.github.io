f54263c260bd4db38bed3140c134d7baf6a486a0
Renata Kilian <renatakiliandev@gmail.com>
2019-06-30 01:12:17 -0300

    Updating files and adding games

---
+++ games/green_madness/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1556753378,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/chão-sheet0.png",
+		"images/green-sheet0.png",
+		"images/esqueleto-sheet0.png",
+		"images/plataforma_chão-sheet0.png",
+		"images/tiro-sheet0.png",
+		"images/hud_vida.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
---
+++ games/the_cube/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1515343989,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/coisinho-sheet0.png",
+		"images/chãozinho-sheet0.png",
+		"images/cidadezinha.png",
+		"images/plataforminha-sheet0.png",
+		"images/plataforminha2-sheet0.png",
+		"images/plataforminha3-sheet0.png",
+		"images/céuzinho-sheet0.png",
+		"images/solzinho-sheet0.png",
+		"images/objetivo-sheet0.png",
+		"images/morte-sheet0.png",
+		"images/moeda-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
